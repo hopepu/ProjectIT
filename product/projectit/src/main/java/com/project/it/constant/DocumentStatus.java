e9e7e4f74794531b374c3767d13e3e85603beb17
hopepu <whdydwo2@gmail.com>
2024-11-29 22:17:46 +0900

    v1.0.0 distribuition backend 1차 완성본

--- product/projectit/src/main/java/com/project/it/constant/DocumentStatus.java
+++
@@ -1,6 +0,0 @@
-package com.project.it.constant;
-
-public enum DocumentStatus {
-    // 결재 요청, 완료,    반려
-    REQUESTED, APPROVED, REJECTED
-}
--- product/projectit/src/main/java/com/project/it/constant/DocumentVisibility.java
+++
@@ -1,6 +0,0 @@
-package com.project.it.constant;
-
-public enum DocumentVisibility {
-    // 공개   비공개
-    PUBLIC, PRIVATE;
-}
